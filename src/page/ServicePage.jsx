6e9b1377e36e54a423ee990cf555474205682282
bastianar <bastianrmdhn12@gmail.com>
2025-05-12 10:17:58 +0700

    Menambahkan animasi on scroll

--- src/page/ServicePage.jsx
+++ src/page/ServicePage.jsx
@@ -7,7 +7,17 @@ const ServicePage = () => {
 
       <div className="ourService bg-white w-full">
         <div className="container mx-auto">
-          <div className="flex flex-col items-center justify-center py-10 sm:py-15 lg:py-25">
+          <div
+            className="flex flex-col items-center justify-center py-10 sm:py-15 lg:py-25"
+            data-aos="fade-up"
+            data-aos-offset="100"
+            data-aos-delay="0"
+            data-aos-duration="600"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="false"
+            data-aos-once="false"
+            data-aos-anchor-placement="bottom-bottom"
+          >
             <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-center text-black">OUR SERVICES</h1>
             <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-4xl text-center font-light mt-4 text-black">
               PESONA Spa provide healing massages for body and circulation.
@@ -23,31 +33,209 @@ const ServicePage = () => {
         <div className="container mx-auto ">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-10 px-4 2xl:px-30 py-10 w-full justify-items-center">
             <div className="service-item w-70 xl:w-100 justify-items-center-safe text-center">
-              <img src="/public/asset/tv-2/Reflexology.png" alt="" className="w-90 h-auto rounded-lg mb-4" />
-              <h2 className="text-2xl xl:text-3xl font-bold text-black">Reflexology</h2>
-              <p className="text-lg text-gray-600 mt-2">
+              <img
+                src="/public/asset/tv-2/Reflexology.png"
+                alt=""
+                className="w-90 h-auto rounded-lg mb-4"
+                data-aos="flip-up"
+                data-aos-offset="100"
+                data-aos-delay="1000"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+              />
+              <h2
+                className="text-2xl xl:text-3xl font-bold text-black"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Reflexology
+              </h2>
+              <p
+                className="text-lg text-gray-600 mt-2"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
                 Unlock the healing power within. <br /> Our reflexology treatments target key points on your feet to promote wellness and address various healt concerns.
               </p>
             </div>
             <div className="service-item w-70 xl:w-100 justify-items-center-safe text-center">
-              <img src="/public/asset/tv-2/Body Massage.png" alt="" className="w-90 h-auto rounded-lg mb-4" />
-              <h2 className="text-2xl xl:text-3xl font-bold text-black">Full Body Massage</h2>
-              <p className="text-lg text-gray-600 mt-2">Transform your well-being with our signature full body massage. Let our expert touch release tension and restore harmony to your body & mind.</p>
+              <img
+                src="/public/asset/tv-2/Body Massage.png"
+                alt=""
+                className="w-90 h-auto rounded-lg mb-4"
+                data-aos="flip-up"
+                data-aos-offset="100"
+                data-aos-delay="1000"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+              />
+              <h2
+                className="text-2xl xl:text-3xl font-bold text-black"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Full Body Massage
+              </h2>
+              <p
+                className="text-lg text-gray-600 mt-2"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Transform your well-being with our signature full body massage. Let our expert touch release tension and restore harmony to your body & mind.
+              </p>
             </div>
             <div className="service-item w-70 xl:w-100 justify-items-center-safe text-center">
-              <img src="/public/asset/tv-2/Body Scrub.png" alt="" className="w-90 h-auto rounded-lg mb-4" />
-              <h2 className="text-2xl xl:text-3xl font-bold text-black">Full Body Massage + Scrub</h2>
-              <p className="text-lg text-gray-600 mt-2">Treat yourself to the complete spa include: a soothing full body massage followes by a refreshing scrub. Unwind, detoxify, and reveal your skin’s natural beauty.</p>
+              <img
+                src="/public/asset/tv-2/Body Scrub.png"
+                alt=""
+                className="w-90 h-auto rounded-lg mb-4"
+                data-aos="flip-up"
+                data-aos-offset="100"
+                data-aos-delay="1000"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+              />
+              <h2
+                className="text-2xl xl:text-3xl font-bold text-black"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Full Body Massage + Scrub
+              </h2>
+              <p
+                className="text-lg text-gray-600 mt-2"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Treat yourself to the complete spa include: a soothing full body massage followes by a refreshing scrub. Unwind, detoxify, and reveal your skin’s natural beauty.
+              </p>
             </div>
             <div className="service-item w-70 xl:w-100 justify-items-center-safe text-center">
-              <img src="/public/asset/tv-2/Kerokan.png" alt="" className="w-90 h-auto rounded-lg mb-4" />
-              <h2 className="text-2xl xl:text-3xl font-bold text-black">Coint Therapy</h2>
-              <p className="text-lg text-gray-600 mt-2">Discover traditional Coint Therapy (Kerokan) ata PESONA SPA for natural wellness.</p>
+              <img
+                src="/public/asset/tv-2/Kerokan.png"
+                alt=""
+                className="w-90 h-auto rounded-lg mb-4"
+                data-aos="flip-up"
+                data-aos-offset="100"
+                data-aos-delay="1000"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+              />
+              <h2
+                className="text-2xl xl:text-3xl font-bold text-black"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Coint Therapy
+              </h2>
+              <p
+                className="text-lg text-gray-600 mt-2"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Discover traditional Coint Therapy (Kerokan) ata PESONA SPA for natural wellness.
+              </p>
             </div>
             <div className="service-item w-70 xl:w-100 justify-items-center-safe text-center">
-              <img src="/public/asset/tv-2/Face Massage.png" alt="" className="w-90 h-auto rounded-lg mb-4" />
-              <h2 className="text-2xl xl:text-3xl font-bold text-black">Face Therapy</h2>
-              <p className="text-lg text-gray-600 mt-2">Experience the rejuvenating and tension-relieving benefits of Face Therapy (Totok Wajah) at PESONA SPA.</p>
+              <img
+                src="/public/asset/tv-2/Face Massage.png"
+                alt=""
+                className="w-90 h-auto rounded-lg mb-4"
+                data-aos="flip-up"
+                data-aos-offset="100"
+                data-aos-delay="1000"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom"
+              />
+              <h2
+                className="text-2xl xl:text-3xl font-bold text-black"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Face Therapy
+              </h2>
+              <p
+                className="text-lg text-gray-600 mt-2"
+                data-aos="fade-up"
+                data-aos-offset="100"
+                data-aos-delay="0"
+                data-aos-duration="600"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+              >
+                Experience the rejuvenating and tension-relieving benefits of Face Therapy (Totok Wajah) at PESONA SPA.
+              </p>
             </div>
           </div>
         </div>
